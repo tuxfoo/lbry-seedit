fe23cae38e4fd847e0d54fc1d6111b0c9733090a
tuxfoo <tuxfoo@librem.one>
2020-11-16 09:21:43 +1300

    Start cron in start script

--- seedit/core/checkmount.sh
+++ seedit/core/checkmount.sh
@@ -2,7 +2,7 @@
 
 ## TODO: Make a bit more aware of the run mode of this appliance in case there is ever a test mode enabled in the start.sh
 mountpoint=/home/lbrynet
-
+declare -p | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' > /container.env
 # Copy the python scripts, as this cannot not be done via docker.
 if [ ! -f "/home/lbrynet/lbry-seedit.py" ]; then
   cp /seedit/lbry-seedit.py /home/lbrynet/lbry-seedit.py
@@ -15,5 +15,7 @@ if ! grep -qs ".* $mountpoint " /proc/mounts; then
     ## TODO: We should have documentation that this error references directly with a URL as to why it won't run without a volume.
     exit 1
 else
+    crontab /etc/cron.d/lbrycron
+    cron
     bash -c "$*"
 fi
